f204f48b988b07923b6ca91cfa1376bf922e1f68
dianavile <diana.vile@gmail.com>
2020-11-23 15:47:14 +0100

    Pass data through props

--- react-tictactoe/src/index.js
+++ react-tictactoe/src/index.js
@@ -4,19 +4,21 @@ import './index.css';
 //import App from './App';
 //import reportWebVitals from './reportWebVitals';
 
+//Square component
 class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
 }
 
+//Board component
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
@@ -45,6 +47,7 @@ class Board extends React.Component {
   }
 }
 
+//Game component
 class Game extends React.Component {
   render() {
     return (
