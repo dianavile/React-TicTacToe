8b3f48fb233e74c88a1078776e66da732c4beeae
dianavile <diana.vile@gmail.com>
2020-11-24 09:42:52 +0100

    raies the state to board

--- react-tictactoe/src/index.js
+++ react-tictactoe/src/index.js
@@ -4,25 +4,15 @@ import './index.css';
 //import App from './App';
 //import reportWebVitals from './reportWebVitals';
 
-//Square component
+//Square component (= controlled component)
 class Square extends React.Component {
-    // TODO: remove the constructor
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
-    // TODO: use onClick={this.props.onClick}
-    // TODO: replace this.state.value with this.props.value
     return (
       <button
         className="square"
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -35,10 +25,21 @@ class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
     }
+   }
+  
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
