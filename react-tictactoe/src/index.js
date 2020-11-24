209a7fee6c4b34416c3d8fb8c9120f8358d9e881
dianavile <diana.vile@gmail.com>
2020-11-24 12:43:02 +0100

    Debug & refactor tictactoegame

--- react-tictactoe/src/index.js
+++ react-tictactoe/src/index.js
@@ -70,18 +70,18 @@ class Game extends React.Component {
     this.setState({
       history: history.concat([
         {
-        squares: squares,
+        squares: squares
         }
       ]),
       stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
+      xIsNext: !this.state.xIsNext
     });
   }
 
   jumpTo(step) {
     this.setState({
       stepNumber: step,
-      xIsNext: (step % 2) === 0,
+      xIsNext: (step % 2) === 0
     });
   }
 
